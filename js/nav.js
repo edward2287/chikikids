7bbc242a1dae13e4f6ff862bfec551b996f99d05
edward2287 <119518465+edward2287@users.noreply.github.com>
2024-07-24 01:43:21 -0400

    pagina al 90% lista index

--- js/nav.js
+++ js/nav.js
@@ -2,5 +2,44 @@
 let loginForm = document.querySelector('.login-form');
 
 document.querySelector('#login-btn').onclick = () => {
-    loginForm.classList.toggle('active')
+    loginForm.classList.toggle('active');
+    navbar.classList.remove('active');
+}
+
+// ocultar nav al bajar con el scroll
+
+let ubicacionPrincipal = window.scrollY;
+let $header = document.querySelector("#header");
+
+window.addEventListener("scroll", function() {
+    let ubicacionActual = window.scrollY;
+    /* console.log(ubicacionActual) */
+
+    if(ubicacionPrincipal >= ubicacionActual) {
+        $header.style.top = "0px"
+    }
+    else {
+        $header.style.top = "-100px"
+    }
+
+
+    ubicacionPrincipal = ubicacionActual;
+
+    
+})
+
+/* activacion de menu hamburguesa */
+
+let navbar = document.querySelector('.navbar');
+
+document.querySelector('#menu-btn').onclick = () =>{
+    navbar.classList.toggle('active');
+    loginForm.classList.remove('active');
+}
+
+/* desactivar la ventana de login y la hamburguesa al dar scroll  */
+
+window.onscroll = () =>{
+    loginForm.classList.remove('active');
+    navbar.classList.remove('active');
 }
